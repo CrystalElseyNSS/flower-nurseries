6ecbea3ce1a49fd850192c25a08b7c1d1077447c
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-04-08 15:09:46 -0500

    added distributor data and refactored retailer list to include distributor

--- scripts/retailers/RetailerList.js
+++ scripts/retailers/RetailerList.js
@@ -1,12 +1,15 @@
 import { useRetailers } from "./RetailerDataProvider.js"
+import { useDistributors } from "../distributors/DistributorDataProvider.js"
 import { Retailer } from "./Retailer.js"
 
 const contentTarget = document.querySelector(".retailerContainer")
 
 const renderRetailers = (retailersToRender) => {
+    const allDistributors = useDistributors()
     contentTarget.innerHTML = retailersToRender.map(
         (retailerObject) => {
-            return Retailer(retailerObject)
+            const foundDistributor = allDistributors.find (distributor => distributor.id === retailerObject.distributorId)
+            return Retailer(retailerObject, foundDistributor)
         }
     ).join("")
 }
